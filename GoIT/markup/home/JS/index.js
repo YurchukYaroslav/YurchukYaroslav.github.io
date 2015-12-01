7ed0fab0910853fe38796cdba0b2123deb6e098b
YurchukYaroslav <urchuk.yaroslav@gmail.com>
2015-12-01 22:04:34 +0200

    changed js-file

--- GoIT/markup/home/JS/index.js
+++ GoIT/markup/home/JS/index.js
@@ -1,6 +1,7 @@
 /**
  * Created by Администратор on 12/1/2015.
  */
+    /*
 var x = prompt('введите число');
 var n = prompt('введите степень');
 
@@ -18,3 +19,24 @@ function pow(a, n) {
     }
     var powResult = pow(x, n);
 }
+*/
+ var arr = [];
+ for (var i = 0; i < 5; i++) {
+     arr[i] = prompt('Enter user name');
+ }
+console.log(arr);
+
+var userName = prompt('Enter your name');
+var flag = false;
+
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === userName) {
+        flag = true;
+
+    }
+}
+ if (flag) {
+     alert(userName + ' confirm!');
+ } else {
+     alert('User not exist!');
+ }
