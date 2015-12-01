1028ab067cf9dd6d97b738a185bff94bd9db3092
YurchukYaroslav <urchuk.yaroslav@gmail.com>
2015-12-01 21:50:13 +0200

    changed js-file

---
+++ GoIT/markup/home/JS/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Администратор on 12/1/2015.
+ */
+var x = prompt('введите число');
+var n = prompt('введите степень');
+
+var result = 1;
+
+for (var i = 0; i < n; i++) {
+    result = result * x;
+}
+console.log(result);
+
+function pow(a, n) {
+    var result = 1;
+    for (var i = 0; i < b; i++ ) {
+        result = result * a;
+    }
+    var powResult = pow(x, n);
+}
