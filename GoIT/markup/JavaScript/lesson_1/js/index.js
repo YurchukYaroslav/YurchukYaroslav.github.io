8c6a02008856f6c76d8c9ecaba7832af2ece5acd
YurchukYaroslav <urchuk.yaroslav@gmail.com>
2016-01-30 12:26:44 +0200

    added new js files

---
+++ GoIT/markup/JavaScript/lesson_1/js/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Администратор on 1/23/2016.
+ */
+function pow(x, n) {
+  var result = x;
+  for (var i = 1; i < n; i++) {
+  result += x;
+  }
+  return result;
+};
+
+var x = prompt('add a number', '');
+var n = prompt('add an exponent', '');
+
+if(n <= 1) {
+  alert('Exponent ' + n + ' is not avalible, enter number > 1');
+} else {
+  alert( pow(x, n));
+}
+
+
