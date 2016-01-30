ce762a2fd40cd3d90e8fa9b2b703af8af5768a50
YurchukYaroslav <urchuk.yaroslav@gmail.com>
2016-01-30 12:43:04 +0200

    Update index.js

--- GoIT/markup/home/JS/index.js
+++ GoIT/markup/home/JS/index.js
@@ -1,25 +1,4 @@
-/**
- * Created by Администратор on 12/1/2015.
- */
-    /*
-var x = prompt('введите число');
-var n = prompt('введите степень');
 
-var result = 1;
-
-for (var i = 0; i < n; i++) {
-    result = result * x;
-}
-console.log(result);
-
-function pow(a, n) {
-    var result = 1;
-    for (var i = 0; i < b; i++ ) {
-        result = result * a;
-    }
-    var powResult = pow(x, n);
-}
-*/
  var arr = [];
  for (var i = 0; i < 5; i++) {
      arr[i] = prompt('Enter user name');
